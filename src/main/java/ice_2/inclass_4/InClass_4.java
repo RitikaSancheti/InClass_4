2facbf277ccaf247a844fef9b15bd2fd6a4d333c
Ritika Sancheti <sanchetr@sheridancollege.ca>
2026-02-05 17:50:13 -0500

    Add method Added

---
+++ src/main/java/ice_2/inclass_4/InClass_4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ice_2.inclass_4;
+
+/**
+ *
+ * @author ritikasancheti
+ */
+public class InClass_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+}
