9c0aed860a1e317888ef32960090cd51d195e54a
Ritika Sancheti <sanchetr@sheridancollege.ca>
2026-02-05 17:55:55 -0500

    Add and Multiply method Added

--- src/main/java/ice_2/inclass_4/InClass_4.java
+++ src/main/java/ice_2/inclass_4/InClass_4.java
@@ -14,5 +14,11 @@ public class InClass_4 {
         System.out.println("Hello World!");
     }
     
+    public double add(double a, double b){
+        return a+b;
+    }
     
+    public double multiply(double a, double b){
+        return a*b;
+    }
 }
